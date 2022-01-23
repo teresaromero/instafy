57d22cee8594ecad824370056aca79ccfdcb242b
Teresa Romero <hello@teresaromero.dev>
2022-01-23 12:05:02 +0100

    ✨ feat: added login/callback

--- api/app/api.py
+++ api/app/api.py
@@ -1,6 +1,8 @@
+from http import HTTPStatus
 from fastapi import Depends, FastAPI
 from fastapi.responses import RedirectResponse, JSONResponse
 from functools import lru_cache
+import requests
 
 from .config import Settings
 
@@ -30,7 +32,23 @@ async def get_login(settings: Settings = Depends(get_settings)) -> dict:
     return RedirectResponse(url)
 
 
-@app.post("/login/callback")
-async def post_login_callback(settings: Settings = Depends(get_settings)) -> dict:
+@app.get("/login/callback")
+async def post_login_callback(code: str, state: str, settings: Settings = Depends(get_settings)) -> dict:
 
-    return JSONResponse({"error": "WIP"})
+    if state != settings.ig_api_state:
+        return JSONResponse({"error": "invalid origin"}, HTTPStatus.UNAUTHORIZED)
+
+    base_url = "https://api.instagram.com/oauth/access_token"
+    data = {
+        "client_id":     settings.ig_api_client_id,
+        "client_secret": settings.ig_api_client_secret,
+        "grant_type":    settings.ig_api_grant_type,
+        "redirect_uri":  settings.ig_api_redirect_url,
+        "code":          code,
+    }
+
+    response = requests.post(url=base_url, data=data)
+    body = response.json()
+    if response.status_code == HTTPStatus.OK:
+        return JSONResponse(body)
+    return JSONResponse({"err": body}, HTTPStatus.INTERNAL_SERVER_ERROR)
